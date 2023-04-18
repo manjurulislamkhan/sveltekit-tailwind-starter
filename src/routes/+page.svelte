c254132b2304b5d4eb9fd4af45cc1aa347b873a3
manjurulislamkhan <manjurulislamkhanpro@gmail.com>
2023-04-18 19:37:56 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-screen h-screen bg-gradient-to-tr from-lime-500 to-lime-300 p-20">
+	<h1 class="text-center font-bold text-slate-800 text-3xl mt-20">
+		Welcome to SvelteKit, configured with Tailwindcss.
+	</h1>
+	<h2 class="text-center font-medium text-slate-800 text-2xl mt-10">Now go make something awesome!</h2>
+	<p class="text-center mt-10"> Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
